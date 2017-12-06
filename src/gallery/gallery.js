fa3408621f161a78dc184317b726e9fdd8d1d95f
anyruizd <anyruizd@gmail.com>
2017-12-06 16:53:00 -0500

    show images and changes

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -1,9 +1,9 @@
-// ------------------------------------ Markdown
+// 1. ------------------------------------ Set template
 
-//import data from './galleryData.js'
+import data from './galleryData.js'
 
 const galleryTemplate = {
-  shell: (
+  containers: (
     `<ul class="gallery__images"></ul>
      <div class ="gallery__controls">
       <button class="gallery__control gallery-control__right">&#62</button>
@@ -13,12 +13,12 @@ const galleryTemplate = {
   ),
   dot: (
     `<li class="gallery__dot">
-      <button class="gallery__dot-button" data-index="{index}"></button>
+      <button class="gallery-dot__button"></button>
     </li>`
   )
 }
 const node = document.querySelector('.gallery')
-node.innerHTML = galleryTemplate.shell // to de DOM
+node.innerHTML = galleryTemplate.containers // to de DOM
 
 // bring DOM references of elements created
 const galleryElements = {}
@@ -26,65 +26,81 @@ galleryElements.galleryImages = node.querySelector('.gallery__images')
 galleryElements.galleryControlRight = node.querySelector('.gallery-control__right')
 galleryElements.galleryControlLeft = node.querySelector('.gallery-control__left')
 galleryElements.galleryDots = node.querySelector('.gallery__dots')
-console.log('galleryElements: ', galleryElements)
 
-/* // Set images
-// Falta poner selectedClass y url
-const imagesTemplate = data.map(() => {
+// 2. ------------------------------------ Set images
+// TODO: Falta poner selectedClass
+const imagesTemplate = data.map(({url}, index) => {
+  return (
     `<li>
-      <img class="gallery-images__element" src=""/>
-    </li>`}
-  ).join('')
-console.log('imagesTemplate') */
-/* this.elements.imagesContainer.innerHTML = imagesHTML
-this.elements.galleryItems = this.node.querySelectorAll('.gallery__image-container')
- */
-// // Set Dots
-/* const dotsHTML = Array.from(Array(data.length)).map(galleryTemplate.dot.replace('{index}', index)).join('')
-console.log('data.length: ',data.length) */
-/* // ------------------------------------ Controlers
-const leftArrow = document.querySelector('.gallery-control__left')
-const rightArrow = document.querySelector('.gallery-control__right')
-const imagesList = document.querySelectorAll('.gallery-images__element')
-let currentIndex = 0
-imagesList[currentIndex].classList.add('gallery-images__element--showing')
-leftArrow.classList.add('gallery__control--disabled')
+      <img class="gallery-images__element" src="${url}"/>
+    </li>`
+  )
+}).join('')
 
-leftArrow.addEventListener('click', goPrevious)
-rightArrow.addEventListener('click', goNext)
+galleryElements.galleryImages.innerHTML = imagesTemplate
+galleryElements.imagesList = document.querySelectorAll('.gallery-images__element')
+
+// 3. ------------------------------------ Set dots
+
+const dotsTemplate = data.map((_, index) => {
+  return (
+    `<li class="gallery__dot">
+      <button class="gallery-dot__button"></button>
+    </li>`
+  )
+}).join('')
+
+galleryElements.galleryDots.innerHTML = dotsTemplate
+galleryElements.dotsList = document.querySelectorAll('.gallery-dot__button')
+
+// ------------------------------------ Arrow Controlers
+
+let currentIndex = 0
+galleryElements.imagesList[currentIndex].classList.add('gallery-images__element--showing')
+galleryElements.galleryControlLeft.classList.add('gallery__control--disabled')
+galleryElements.galleryControlLeft.addEventListener('click', goPrevious)
+galleryElements.galleryControlRight.addEventListener('click', goNext)
 
 function goPrevious () {
   let testIndex = currentIndex - 1
-  if (checkStatus(testIndex)) {
-    imagesList[currentIndex].classList.remove('gallery-images__element--showing')
-    currentIndex = testIndex
-    imagesList[currentIndex].classList.add('gallery-images__element--showing')
-  }
+  checkStatus(testIndex) && (currentIndex = updateStatus(currentIndex, testIndex))
 }
 
 function goNext () {
   let testIndex = currentIndex + 1
-  if (checkStatus(testIndex)) {
-    imagesList[currentIndex].classList.remove('gallery-images__element--showing')
-    currentIndex = testIndex
-    imagesList[currentIndex].classList.add('gallery-images__element--showing')
-  }
+  checkStatus(testIndex) && (currentIndex = updateStatus(currentIndex, testIndex))
 }
 
 function checkStatus (index) {
   const isPositive = index >= 0
-  const isLessThanLength = index < imagesList.length
+  const isLessThanLength = index < galleryElements.imagesList.length
   const isDifferentThanCurrent = index !== currentIndex
   if (index === 0) {
-    leftArrow.classList.add('gallery__control--disabled')
-  } else if (index === imagesList.length - 1) {
-    rightArrow.classList.add('gallery__control--disabled')
+    galleryElements.galleryControlLeft.classList.add('gallery__control--disabled')
+  } else if (index === galleryElements.imagesList.length - 1) {
+    galleryElements.galleryControlRight.classList.add('gallery__control--disabled')
   } else {
-    leftArrow.classList.remove('gallery__control--disabled')
-    rightArrow.classList.remove('gallery__control--disabled')
+    galleryElements.galleryControlLeft.classList.remove('gallery__control--disabled')
+    galleryElements.galleryControlRight.classList.remove('gallery__control--disabled')
   }
   if (isPositive && isLessThanLength && isDifferentThanCurrent) {
     return true
-  } else return false
+  }
 }
- */
+
+function updateStatus (current, test) {
+  galleryElements.imagesList[current].classList.remove('gallery-images__element--showing')
+  current = test
+  galleryElements.imagesList[current].classList.add('gallery-images__element--showing')
+  return current
+}
+
+// ------------------------------------ Dots Controlers
+galleryElements.galleryDots.addEventListener('click', changeDot)
+
+function changeDot (event) {
+  var clickedElement = event.target
+  if (clickedElement.classList.contains('gallery-dot__button')) {
+    console.log('clickedElement button!!')
+  }
+}
