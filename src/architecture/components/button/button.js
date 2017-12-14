448a51795b84aaec44cd83d0b3e96ceeea37b375
anyruizd <anyruizd@gmail.com>
2017-12-14 15:20:26 -0500

    add architecture components

--- src/architecture/components/button/button.js
+++ src/architecture/components/button/button.js
@@ -1,13 +1,13 @@
 export class Button {
-    constructor (selector, text, callback) {
-        this.callback = callback
-        this.text = text
-        this.node = document.querySelector(selector)
-        this.buildUI()
-    }
+  constructor (selector, text, callback) {
+    this.callback = callback
+    this.text = text
+    this.node = document.querySelector(selector)
+    this.buildUI()
+  }
 
-    buildUI () {
-        this.node.innerText = this.text
-        this.node.addEventListener('click', this.callback)
-    }
+  buildUI () {
+    this.node.innerText = this.text
+    this.node.addEventListener('click', this.callback)
+  }
 }
--- src/architecture/components/display/display.js
+++ src/architecture/components/display/display.js
@@ -1,9 +1,9 @@
 export class Display {
-    constructor(selector, text){
-        this.text = text
-        this.node = document.querySelector(selector)
-    }
-    setText(){
-        this.node.innerHTML = this.text
-    }
+  constructor (selector, text) {
+    this.text = text
+    this.node = document.querySelector(selector)
+  }
+  setText () {
+    this.node.innerHTML = this.text
+  }
 }
