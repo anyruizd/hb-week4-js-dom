51cf46addd9a055e0f75ccaae6053e0229536995
anyruizd <anyruizd@gmail.com>
2018-01-17 11:34:23 -0500

    Add card flipping effects

--- src/movies/components/grid/grid.js
+++ src/movies/components/grid/grid.js
@@ -8,29 +8,30 @@ export class Grid {
   setTemplate (data) {
     this.gridElements.template = data.map(({title, year, url, category, director, description}, index) => {
       return (
-        `<li class = "grid__card" data-card-index = "${index}" data-card-id = "${category.toLowerCase()}">
-          <div class = "grid__card grid__card--front">
-            <img class = "grid__card-element" src="${url}">
-            <ul class = "grid__card-infos">
-              <li class = "grid__card-info">
-                <h2>
-                  <p>Title:</p>
-                  <p>Genre:</p>
-                  <p>Year:</p>
-                  <p>Director:</p>
-                </h2>
+        `<li class = "grid__card-container" data-card-id = "${category.toLowerCase()}">
+        <div class = "grid__card"> 
+          <div class = "grid__card--front">
+              <img class = "grid__card-element" src="${url}">
+              <ul class = "grid__card-infos">
+                <li class = "grid__card-info">
+                  <h2>
+                    <p>Title:</p>
+                    <p>Genre:</p>
+                    <p>Year:</p>
+                    <p>Director:</p>
+                  </h2>
+                </li>
+                <li class = "grid__card-info">
+                  <h3>
+                  <p>${title}</p>
+                  <p>${category}</p>
+                  <p>${year}</p>
+                  <p>${director}</p>
+                  </h3>
               </li>
-              <li class = "grid__card-info">
-                <h3>
-                <p>${title}</p>
-                <p>${category}</p>
-                <p>${year}</p>
-                <p>${director}</p>
-                </h3>
-            </li>
-          </ul>
-          </div>
-          <div class = "grid__card grid__card--back">
+            </ul>
+            </div>
+          <div class = "grid__card--back">
           <h4>${title}</h4>
           <p>${description}</p>
           </div>
@@ -38,34 +39,39 @@ export class Grid {
       )
     }).join('')
     this.node.innerHTML = this.gridElements.template
-    this.gridElements.cardsList = Array.from(this.node.querySelectorAll('.grid__card'))
+    this.gridElements.cardsList = Array.from(this.node.querySelectorAll('.grid__card-container'))
   }
   filterCardByCategory (category) {
     this.gridElements.cardsList.map((element) => {
+      element.classList.remove('grid__card--clicked')
       if (element.dataset.cardId !== category) {
-        element.classList.remove('grid__card--showing')
-        element.classList.add('grid__card--hidden')
+        element.classList.remove('grid__card-container--showing')
+        element.classList.add('grid__card-container--hidden')
       }
       if (element.dataset.cardId === category) {
-        element.classList.remove('grid__card--hidden')
-        element.classList.add('grid__card--showing')
+        element.classList.remove('grid__card-container--hidden')
+        element.classList.add('grid__card-container--showing')
       }
     })
   }
   showAllCategories () {
     this.gridElements.cardsList.map((element) => {
-      element.classList.remove('grid__card--hidden')
-      element.classList.add('grid__card--showing')
+      element.classList.remove('grid__card--clicked')
+      element.classList.remove('grid__card-container--hidden')
+      element.classList.add('grid__card-container--showing')
     })
   }
 
-  gridEvents () { // no funciona :(
+  gridEvents () {
     this.node.addEventListener('click', (event) => {
-      // this.node.classList.toggle('grid__card--clicked')
-      const clickedElement = event.target
-      const parentElement = clickedElement.closest('.grid__card')
-      parentElement.classList.toggle('grid__card--clicked')
-      console.log(parentElement)
+      const clickedElement = event.target.closest('.grid__card-container')
+      this.gridElements.cardsList.map((element) => {
+        if (element === clickedElement) {
+          clickedElement.classList.toggle('grid__card--clicked')
+        } else {
+          element.classList.remove('grid__card--clicked')
+        }
+      })
     })
   }
 }
